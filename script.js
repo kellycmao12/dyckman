58e56c1dbc9e761fa6d7099b47b1ced5dcf856f9
kellycmao12 <74526299+kellycmao12@users.noreply.github.com>
2022-04-27 20:39:29 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,30 +6,68 @@ window.onload = () => {
         if(firstLoc) {
 			firstLoc = false;
             alert(`Got GPS: you are at: ${e.detail.position.longitude} ${e.detail.position.latitude}`);
-            setPos(e.detail.position.longitude, e.detail.position.latitude);
+            // setPos(e.detail.position.longitude, e.detail.position.latitude);
+            let places = staticLoadPlaces();
+            renderPlaces(places, e.detail.position.longitude, e.detail.position.latitude);    
         }
-    });    
+    });
+    
 };
 
-function setPos(longitude,latitude) {
-
-        for (let i = 0; i < 10; i++) {
-            const box = document.createElement("a-box");
-            box.setAttribute('scale', {
-                x: 10, 
-                y: 10,
-                z: 10
-            });
-            box.setAttribute('material', {
-                color: 'red'
-            });
-            box.setAttribute('gps-projected-entity-place', {
-                latitude: latitude + 0.0001 * i,
-                longitude: longitude - 0.0001 * i
-            });
-
-            const sceneEl = document.querySelector("a-scene");
-            sceneEl.appendChild(box);
-        }
-        
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'cherry',
+            link: './assets/cherries/scene.gltf'
+        },
+        {
+            name: 'magnemite',
+            link: './assets/magnemite/scene.gltf'
+        },
+    ];
 }
+
+function renderPlaces(places, longitude, latitude) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = latitude + random(-0.0001, 0.0001);
+        let longitude = longitude + random(-0.0001, 0.0001);
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-projected-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model', './assets/' + place.name + '/scene.gltf');
+        model.setAttribute('rotation', '0 180 0');
+        model.setAttribute('scale', '0.5 0.5 0.5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
+
+// function setPos(longitude,latitude) {
+
+//         for (let i = 0; i < 10; i++) {
+            
+//             box.setAttribute('scale', {
+//                 x: 10, 
+//                 y: 10,
+//                 z: 10
+//             });
+//             box.setAttribute('material', {
+//                 color: 'red'
+//             });
+//             const box = document.createElement("a-box");
+//             box.setAttribute('gps-projected-entity-place', {
+//                 latitude: latitude + 0.0001 * i,
+//                 longitude: longitude - 0.0001 * i
+//             });
+
+//             const sceneEl = document.querySelector("a-scene");
+//             sceneEl.appendChild(box);
+//         }
+        
+// }
