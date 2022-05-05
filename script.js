c10edb54e6d4caf9cf01a7c22cef7ac2fc88a397
kellycmao12 <74526299+kellycmao12@users.noreply.github.com>
2022-05-05 07:21:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ let firstLoc = true;
 function staticLoadPlaces() {
     return [
         {
-            name: 'apple',
-            link: './assets/apple/scene.gltf'
+            name: 'cherry',
+            link: './assets/cherry/scene.gltf'
         },
-        {
-            name: 'apple tree',
-            link: './assets/apple tree/scene.gltf'
-        },
+        // {
+        //     name: 'apple',
+        //     link: './assets/apple/scene.gltf'
+        // },
+        // {
+        //     name: 'apple',
+        //     link: './assets/apple/scene.gltf'
+        // },
+        // {
+        //     name: 'apple tree',
+        //     link: './assets/apple tree/scene.gltf'
+        // },
     ];
 }
 
 function renderPlaces(places, longitude, latitude) {
-    let scene = document.querySelector('a-scene');
+    let scene = document.querySelector('a-scene-apple');
 
     places.forEach((place) => {
         let randNum = Math.random()/10000;
@@ -26,7 +34,6 @@ function renderPlaces(places, longitude, latitude) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-projected-entity-place', `latitude: ${randLatitude}; longitude: ${randLongitude};`);
         model.setAttribute('gltf-model', place.link);
-        model.setAttribute('rotation', '0 180 0');
         model.setAttribute('scale', '0.5 0.5 0.5');
 
         model.addEventListener('loaded', () => {
@@ -37,22 +44,72 @@ function renderPlaces(places, longitude, latitude) {
     });
 }
 
-function startGame() {
-    console.log("start game");
+function startIntro() {
     toggleScreen('start-screen', false);
-    toggleScreen('main-screen', true);
+    toggleScreen('intro-video-screen', true);
+    const video = document.getElementById('intro-video');
+    video.play();
+    video.addEventListener('ended', showNextButton, false);
+    function showNextButton(e) {
+        const nextBtn = document.getElementById('next-button');
+        nextBtn.style.display = 'block';
+    }
+}
+
+function startAppleAR() {
+    console.log("start apple AR");
+    toggleScreen('intro-video-screen', false);
+    toggleScreen('apple-ar-screen', true);
     toggleVideo(true);
 
     const camera = document.querySelector('a-camera');
     window.addEventListener("gps-camera-update-position", e => {
         if(firstLoc) {
+            console.log("got first location");
 			firstLoc = false;
             alert(`Got GPS: you are at: ${e.detail.position.longitude} ${e.detail.position.latitude}`);
-            // setPos(e.detail.position.longitude, e.detail.position.latitude);
+            setPosition(e.detail.position.longitude, e.detail.position.latitude);
             let places = staticLoadPlaces();
             renderPlaces(places, e.detail.position.longitude, e.detail.position.latitude);    
         }
     });
+}
+
+function openAppleVideo() {
+    toggleScreen('apple-ar-screen', false);
+    toggleScreen('apple-video-screen', true);
+    const video = document.getElementById('apple-video');
+    video.play();
+    video.addEventListener('ended', showNextButton, false);
+    function showNextButton(e) {
+        const nextBtn = document.getElementById('next-button');
+        nextBtn.style.display = 'block';
+    }
+}
+
+function startArtAR() {
+    toggleScreen('apple-video-screen', false);
+    toggleScreen('art-ar-screen', true);
+    toggleVideo(true);
+
+    const camera = document.querySelector('a-camera');
+    window.addEventListener("gps-camera-update-position", e => {
+        if(firstLoc) {
+            console.log("got first location");
+			firstLoc = false;
+            alert(`Got GPS: you are at: ${e.detail.position.longitude} ${e.detail.position.latitude}`);
+            setPos(e.detail.position.longitude, e.detail.position.latitude);
+            let places = [{ name: 'reggie black', link: './assets/reggie black.obj' }];
+            renderPlaces(places, e.detail.position.longitude, e.detail.position.latitude);    
+        }
+    });
+}
+
+function openArtVideo() {
+    toggleScreen('art-ar-screen', false);
+    toggleScreen('art-video-screen', true);
+    const video = document.getElementById('art-video');
+    video.play();
 }
 
 function toggleScreen(id, toggle) {
@@ -67,13 +124,6 @@ function toggleVideo(toggle) {
         video.setAttribute('style', 'display: block !important; position: absolute; top: 0px; left: 0px;');
     } else {
         video.setAttribute('style', 'display: none !important;');
-        video.play();
     }
-}
-
-function openVideo() {
-    const popup = document.getElementById("asl-button");
-    popup.classList.toggle("show");
-
 }
 
